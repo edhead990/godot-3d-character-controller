06aed8418db7acf3f94825455c552342e8f42b4b
root <175668896+penwick0@users.noreply.github.com>
2026-02-17 21:29:57 +0000

    Strafe and aim mechanics added

--- Camera.gd
+++ Camera.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
 @export var player: CharacterBody3D
+@export var reset_speed: float = 10.0 # Adjust for "snappiness"
 
 const SPEED = 5.0
 const MOUSE_X_SENSITIVITY = 0.5
 const MOUSE_Y_SENSITIVITY = 0.25
 
+var is_resetting: bool = false
+var target_rotation_y: float = 0.0
 var y_offset
 
 # Called when the node enters the scene tree for the first time.
@@ -13,13 +16,11 @@ func _ready():
 	y_offset = global_position.y
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-
 func _input(event):
 	if event is InputEventMouseMotion:
 		rotation.y += deg_to_rad(-event.relative.x * MOUSE_X_SENSITIVITY)
 		rotation.x += deg_to_rad(-event.relative.y * MOUSE_Y_SENSITIVITY)
 		rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(30))
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,10 +28,29 @@ func _process(delta):
 	global_position.y = player.global_position.y + y_offset
 	global_position.z = player.global_position.z
 
-	# Handle input
+	# Handle direction input
 	var input_dir = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
 	var direction = Vector3(input_dir.x, 0, input_dir.y)
 	if direction:
 		rotation.y += -direction.x * (SPEED * delta)
 		rotation.x += -direction.z * (SPEED * delta)
 		rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(30))
+
+	if Input.is_action_just_pressed("camera_reset"):
+		start_camera_reset(player.rotation.y)
+	if is_resetting:
+		_perform_reset_step(delta)
+
+func start_camera_reset(goal_angle: float):
+	target_rotation_y = goal_angle
+	is_resetting = true
+
+func _perform_reset_step(delta):
+	rotation.y = lerp_angle(rotation.y, target_rotation_y, reset_speed * delta)
+	rotation.x = lerp_angle(rotation.x, 0.0, reset_speed * delta)
+	
+	# Stop processing once we are close enough to the target
+	if abs(angle_difference(rotation.y, target_rotation_y)) < 0.01:
+		is_resetting = false
+
+	# TODO: Break reset if the player tries to look around manually?
--- Player.gd
+++ Player.gd
@@ -5,9 +5,12 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var is_aiming = false
+var is_strafing = false
+var strafe_rotation: float = 0.0
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -18,14 +21,39 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+	# Handle aim state
+	if Input.is_action_pressed("aim"):
+		is_aiming = true
+	else:
+		is_aiming = false
+		
+	# Handle strafe
+	if Input.is_action_just_pressed("strafe"):
+		# Set strafe angle to direction character is facing
+		strafe_rotation = rotation.y
+		camera_controller.start_camera_reset(rotation.y)
+	if Input.is_action_pressed("strafe"):
+		is_strafing = true
+	else:
+		is_strafing = false
+
+	# Lock player rotation to camera
+	if is_aiming:
+		rotation.y = camera_controller.rotation.y
+
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, camera_controller.rotation.y).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		if is_strafing:
+			rotation.y = strafe_rotation
+		
 		# Smooth rotation in the movement direction
-		rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), 12.0 * delta)
+		else:
+			rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), 12.0 * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
