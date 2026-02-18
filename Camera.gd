d1bda0904b447627be0ec455456b4e0e8ed95fbf
Edward Bittner <6522402+edhead990@users.noreply.github.com>
2026-02-18 19:20:27 +1030

    Added animation library and basic animations to the player. Basic camera reset.

--- Camera.gd
+++ Camera.gd
@@ -2,15 +2,20 @@ extends Node3D
 
 @export var player: CharacterBody3D
 
-const SPEED = 5.0
-const MOUSE_X_SENSITIVITY = 0.5
-const MOUSE_Y_SENSITIVITY = 0.25
+const JOYSTICK_X_SESITIVITY: float = 2.5
+const JOYSTICK_Y_SESITIVITY: float = 2.0
+const MOUSE_X_SENSITIVITY: float = 0.25
+const MOUSE_Y_SENSITIVITY: float = 0.125
 
-var y_offset
+var initial_position: Vector3
+var initial_rotation: Vector3
+var is_resetting: bool = false
+var reset_to: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	y_offset = global_position.y
+	initial_position = global_position
+	initial_rotation = global_rotation
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
@@ -23,14 +28,25 @@ func _input(event):
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position.x = player.global_position.x
-	global_position.y = player.global_position.y + y_offset
-	global_position.z = player.global_position.z
+	global_position.x = player.global_position.x + initial_position.x
+	global_position.y = player.global_position.y + initial_position.y
+	global_position.z = player.global_position.z + initial_position.z
 
-	# Handle input
-	var input_dir = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
-	var direction = Vector3(input_dir.x, 0, input_dir.y)
-	if direction:
-		rotation.y += -direction.x * (SPEED * delta)
-		rotation.x += -direction.z * (SPEED * delta)
-		rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(30))
+	if Input.is_action_just_pressed("camera_reset"):
+		reset_to = Vector2(initial_rotation.x, player.rotation.y)
+		is_resetting = true
+
+	if is_resetting:
+		if abs(rotation.x - reset_to.x) <= 0.001 and abs(rotation.y - reset_to.y) <= 0.001:
+			is_resetting = false
+		else:
+			rotation.x = lerp_angle(rotation.x, reset_to.x, 20 * delta)
+			rotation.y = lerp_angle(rotation.y, reset_to.y, 20 * delta)
+	else:
+		# Handle input
+		var input_dir = Input.get_vector("camera_left", "camera_right", "camera_up", "camera_down")
+		var direction = Vector3(input_dir.x, 0, input_dir.y)
+		if direction:
+			rotation.y += -direction.x * (JOYSTICK_X_SESITIVITY * delta)
+			rotation.x += -direction.z * (JOYSTICK_Y_SESITIVITY * delta)
+			rotation.x = clamp(rotation.x, deg_to_rad(-89), deg_to_rad(30))
--- Player.gd
+++ Player.gd
@@ -2,12 +2,12 @@ extends CharacterBody3D
 
 @export var camera_controller = Node3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const SPEED: float = 5.0
+const JUMP_VELOCITY: float = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+var previous_direction: Vector3 = Vector3.ZERO
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,13 +21,24 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var direction = Vector3(input_dir.x, 0, input_dir.y).rotated(Vector3.UP, camera_controller.rotation.y).normalized()
-	if direction:
+
+	if camera_controller.is_resetting:
+		direction = Vector3.ZERO
+
+	var valid_x_direction = abs(previous_direction.x - direction.x) < 1.5
+	var valid_z_direction = abs(previous_direction.z - direction.z) < 1.5
+
+	if direction and valid_x_direction and valid_z_direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 		# Smooth rotation in the movement direction
-		rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), 12.0 * delta)
+		# rotation.y = lerp_angle(rotation.y, atan2(-velocity.x, -velocity.z), 12.0 * delta)
+		# Alternative: Instant rotation
+		rotation.y = atan2(-velocity.x, -velocity.z)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	previous_direction = direction
 
 	move_and_slide()
